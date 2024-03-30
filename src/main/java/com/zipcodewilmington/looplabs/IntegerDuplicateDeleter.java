0e9b9440b8227138d496cef52bbb2bac949ea87f
Shaily Modi <shailykunal@gmail.com>
2024-03-30 10:57:07 -0400

    completed removeDuplicates test

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -4,5 +4,52 @@ package com.zipcodewilmington.looplabs;
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
-public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer>
+{
+
+    public IntegerDuplicateDeleter(Integer[] intArray)
+    {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] tempArray = new Integer[array.length];
+        Integer newArrayIndex = 0;
+        Integer counter;
+
+        for (int a : array)
+        {
+            counter = arrayCounter(a);
+            if(counter < maxNumberOfDuplications)
+            {
+                tempArray[newArrayIndex] = a;
+                newArrayIndex ++;
+            }
+        }
+
+        Integer[] finalArray = new Integer[newArrayIndex];
+
+        System.arraycopy(tempArray, 0, finalArray, 0, newArrayIndex);
+        return  finalArray;
+    }
+
+    public Integer[] removeDuplicatesExactly(int number)
+    {
+        return null;
+    }
+
+    public int arrayCounter(int number)
+    {
+        int counter = 0;
+        for (int a : array)
+        {
+            if(a == number)
+            {
+                counter ++;
+            }
+        }
+        return counter;
+    }
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -4,5 +4,30 @@ package com.zipcodewilmington.looplabs;
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
-public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+public final class StringDuplicateDeleter extends DuplicateDeleter<String>
+{
+
+
+    public StringDuplicateDeleter(String[] intArray)
+    {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications)
+    {
+        return null;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications)
+    {
+        return null;
+    }
+
+    public int arrayCounter(String myString)
+    {
+        return 0;
+    }
+
 }
